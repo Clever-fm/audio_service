2c47833b3d62626d3a0da08f0830771036803acf
AlexanderHorielko <s.gorelko1@gmail.com>
2021-08-16 18:43:41 +0300

    fix(assert): remove assert

--- audio_service/lib/audio_service.dart
+++ audio_service/lib/audio_service.dart
@@ -878,7 +878,7 @@ class AudioService {
     AudioServiceConfig? config,
     BaseCacheManager? cacheManager,
   }) async {
-    assert(_cacheManager == null);
+    // assert(_cacheManager == null);
     config ??= const AudioServiceConfig();
     assert(config.fastForwardInterval > Duration.zero);
     assert(config.rewindInterval > Duration.zero);
